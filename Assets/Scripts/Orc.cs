ac707aaadfc32879b1bda14b7efec1bb5feb1c42
Chris <nearman.cj@gmail.com>
2016-08-22 21:38:22 -0700

    Added scripts for movement of enemies toward player.

---
+++ Assets/Scripts/Orc.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Orc : BaseEnemy
+    {
+        public GameObject Target;
+        WalkTowardTargetAction walk;
+
+        //On Update, move toward target.
+        void Awake()
+        {
+            walk = new WalkTowardTargetAction(this, 2.0f);
+            walk.target = Target.GetComponent<BaseEntity>(); //gameObject.GetComponent<BaseEntity>();
+        }
+
+        void FixedUpdate()
+        {
+            walk.PerformAction();
+        }
+    }
+}
---
+++ Assets/Scripts/WalkTowardTargetAction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class WalkTowardTargetAction : Action
+    {
+        private Rigidbody _rigidbody;
+        private float MovementSpeed;
+
+        public BaseEntity target { get; set; }
+
+        public WalkTowardTargetAction(BaseEntity entity, float ms)
+        {
+            _rigidbody = entity.GetComponent<Rigidbody>();
+            MovementSpeed = ms;
+        }
+
+        public override void PerformAction()
+        {
+            var currentPosition = _rigidbody.position;
+            var targetPosition = target.transform.position;
+            var targetVector = (new Vector3(targetPosition.x, 0, targetPosition.z) - new Vector3(currentPosition.x, 0, currentPosition.z));
+            targetVector.Normalize();
+            _rigidbody.MovePosition(currentPosition + ( targetVector * MovementSpeed ) * Time.deltaTime);
+        }
+    }
+}
