925d5f47f5921d62bec6189691ecaf546490ebf4
Adam <imposter110@gmail.com>
2016-08-22 20:54:40 -0700

    Build of touch input

---
+++ Assets/Scripts/TouchControl.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchControl : MonoBehaviour {
+
+    public GameObject testPlayer;
+    int wallMask = 1 << 12;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        foreach (Touch currentTouches in Input.touches)
+        {
+            if (currentTouches.phase == TouchPhase.Ended)
+            {
+                var ray = Camera.main.ScreenPointToRay(currentTouches.position);
+                MoveTo(ray);
+            }
+        }
+    }
+
+    void MoveTo(Ray ray)
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 100f, wallMask))
+        {
+            testPlayer.transform.position = hit.point;
+        }
+    }
+}
