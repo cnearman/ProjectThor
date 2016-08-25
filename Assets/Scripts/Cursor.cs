203fbd1c598be30ea06f32730a3e77fea2ea428f
Adam <imposter110@gmail.com>
2016-08-24 23:06:07 -0700

    Basic movement

---
+++ Assets/Scripts/Cursor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cursor : MonoBehaviour {
+
+    public ParticleSystem bloom;
+
+    public void PlayPart()
+    {
+        bloom.Play();
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
