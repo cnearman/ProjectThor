df976054ac9094486ce392e726aa82d8881982ba
Adam <imposter110@gmail.com>
2016-08-29 21:38:06 -0700

    Now with circles

--- Assets/Scripts/GestureReader.cs
+++ Assets/Scripts/GestureReader.cs
@@ -21,6 +21,8 @@ public class GestureReader : MonoBehaviour {
 
     public GameObject to1;
 
+    public GameObject co1;
+
     void Start()
     {
         grace = Screen.height / 15f;
@@ -40,8 +42,21 @@ public class GestureReader : MonoBehaviour {
         Gesture vLine = new Gesture(vA, "verLine");
         Gesture hLine = new Gesture(hA, "hozLine");
 
+        List<Point> circlePoints = new List<Point>();
 
-        trainers = new Gesture[]{vLine, hLine};
+        for(float i = 0; i < 360f; i += 11.25f)
+        {
+            float x = 10f * Mathf.Cos(i);
+            float y = 10f * Mathf.Sin(i);
+
+            Point tempC = new Point(x, y, 0);
+
+            circlePoints.Add(tempC);
+        }
+
+        Gesture rCircle = new Gesture(circlePoints.ToArray(), "aCircle");
+
+        trainers = new Gesture[]{vLine, hLine, rCircle};
     }
 
     void TapAction(Ray rayT)
@@ -67,6 +82,28 @@ public class GestureReader : MonoBehaviour {
         if (Physics.Raycast(endRay, out hit2, 100f, wallMask))
         {
             lo2.transform.position = hit2.point;
+        }
+    }
+
+    void DrawCircle(Ray origin, Ray rad)
+    {
+        RaycastHit hit1;
+        RaycastHit hit2;
+
+        float fX = 0f;
+        float dX;
+
+        if (Physics.Raycast(origin, out hit1, 100f, wallMask))
+        {
+            co1.transform.position = hit1.point;
+            fX = hit1.point.x;
+        }
+
+        if (Physics.Raycast(rad, out hit2, 100f, wallMask))
+        {
+            dX = Mathf.Abs(fX - hit2.point.x) * 2f;
+            co1.transform.localScale = new Vector3(dX, 0.3f, dX);
+            
         }
     }
 
@@ -114,18 +151,69 @@ public class GestureReader : MonoBehaviour {
                     Point[] tempLine = { firstPoint, lastPoint };
                     Gesture tempGest = new Gesture(tempLine, "tLine");
 
-                    Gesture[] tempTrainers = { trainers[0], trainers[1], tempGest };
+                    Gesture[] tempTrainers = { trainers[0], trainers[1], trainers[2], tempGest };
 
                     //send to PDollar
                     Point[] pointArray = currentPoints.ToArray();
                     Gesture myGesture = new Gesture(pointArray);
                     string nameOfShape = PointCloudRecognizer.Classify(myGesture, tempTrainers);
 
-                    if(nameOfShape == "tLine")
+                    if (nameOfShape == "tLine")
                     {
                         var ray1 = Camera.main.ScreenPointToRay(new Vector3(firstPoint.X, firstPoint.Y, 0f));
                         var ray2 = Camera.main.ScreenPointToRay(new Vector3(lastPoint.X, lastPoint.Y, 0f));
                         LineAction(ray1, ray2);
+                    } else if (nameOfShape == "aCircle") {
+                        Debug.Log("its a circle");
+                        //find origin and radius
+                        float totalX = 0f;
+                        float totalY = 0f;
+
+                        float aveX = 0f;
+                        float aveY = 0f;
+
+                        float minY = pointArray[0].Y;
+                        float maxY = pointArray[0].Y;
+
+                        float minX = pointArray[0].X;
+                        float maxX = pointArray[0].X;
+
+                        for (int j = 0; j < pointArray.Length; j++)
+                        {
+                            totalX += pointArray[j].X;
+                            totalY += pointArray[j].Y;
+
+                            if(pointArray[j].X < minX)
+                            {
+                                minX = pointArray[j].X;
+                            }
+                            if(pointArray[j].X > maxX)
+                            {
+                                maxX = pointArray[j].X;
+                            }
+                            if(pointArray[j].Y < minY)
+                            {
+                                minY = pointArray[j].Y;
+                            }
+                            if(pointArray[j].Y > maxY)
+                            {
+                                maxY = pointArray[j].Y;
+                            }
+
+                        }
+
+                        float myRadius = ((Mathf.Abs((maxX - minX)) + Mathf.Abs((maxY - minY))) / 2f) / 2f;
+
+                        aveX = totalX / pointArray.Length;
+                        aveY = totalY / pointArray.Length;
+
+                        Debug.Log(totalX + ", " + aveX);
+
+                        var ray3 = Camera.main.ScreenPointToRay(new Vector3(aveX, aveY, 0f));
+                        var ray4 = Camera.main.ScreenPointToRay(new Vector3(aveX + myRadius, aveY, 0f));
+
+                        DrawCircle(ray3, ray4);
+
                     } else if (nameOfShape == "verLine")
                     {
                         testObj.GetComponent<MeshRenderer>().material = mat1;
