a2671918ba93a09fec10fcd2f00aad62ec3e98a1
Adam <imposter110@gmail.com>
2016-09-18 18:53:15 -0700

    Added camara follow and blood when enemy gets hit

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    public GameObject objectTofollow;
+    public float hArea;
+    public float vArea;
+
+    public float vOffset;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(objectTofollow.transform.position.x > transform.position.x + hArea)
+        {
+            transform.position = new Vector3(objectTofollow.transform.position.x - hArea, transform.position.y, transform.position.z);
+        } else if(objectTofollow.transform.position.x < transform.position.x - hArea)
+        {
+            transform.position = new Vector3(objectTofollow.transform.position.x + hArea, transform.position.y, transform.position.z);
+        }
+
+        if(objectTofollow.transform.position.z > (transform.position.z + vOffset) + vArea)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, objectTofollow.transform.position.z - vOffset - vArea);
+        } else if(objectTofollow.transform.position.z < (transform.position.z + vOffset) - vArea)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, objectTofollow.transform.position.z - vOffset + vArea);
+        }
+	}
+}
