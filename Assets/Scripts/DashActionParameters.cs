66ffb515476ad0db60fd84bf89ba365582838b97
Chris <nearman.cj@gmail.com>
2016-09-18 19:00:02 -0700

    Added dashing action to player.

---
+++ Assets/Scripts/DashActionParameters.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class DashActionParameters
+    {
+        public BaseEntity Entity { get; set; }
+        
+        public float DashRange { get; set; }
+
+        public float DashDamage { get; set; }
+
+        public Vector3 Target { get; set; }
+
+        public List<string> Tags { get; set; }
+    }
+}
